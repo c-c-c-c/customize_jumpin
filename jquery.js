bc228b466f111fc6370da607b2a9af071787dd15
4c <4c.mura@gmail.com>
2018-03-03 20:54:53 +0900

    ちょい修正

--- jquery.js
+++ jquery.js
@@ -8,108 +8,6 @@ $(function(){
     });
  });
 
-/*表示*/
-// $(function(){
-//       $(".button_1").click(function(){
-//         $(".botanical_1").css({
-//           "opacity":"1",
-//           "transition":"0.7s",
-//           "top":"70px",
-//           "left": "500px",
-//         });
-//       })
-//     })
-// $(function(){
-//       $(".button_2").click(function(){
-//         $(".botanical_2").css({
-//           "opacity":"1",
-//           "transition":"0.7s",
-//           "top":"100px",
-//           "left": "550px",
-//         });
-//       })
-//     })
-// $(function(){
-//       $(".button_3").click(function(){
-//         $(".botanical_3").css({
-//           "opacity":"1",
-//           "transition":"0.7s",
-//           "top":"100px",
-//           "left": "480px",
-//         });
-//       })
-//     })
-// $(function(){
-//       $(".button_4").click(function(){
-//         $(".botanical_4").css({
-//           "opacity":"1",
-//           "transition":"0.7s",
-//           "top":"80px",
-//           "left": "500px",
-//         });
-//       })
-//     })
-// $(function(){
-//       $(".button_5").click(function(){
-//         $(".botanical_5").css({
-//           "opacity":"1",
-//           "transition":"0.7s",
-//           "top":"80px",
-//           "left": "430px",
-//         });
-//       })
-//     })
-// $(function(){
-//       $(".button_6").click(function(){
-//         $(".botanical_6").css({
-//           "opacity":"1",
-//           "transition":"0.7s",
-//           "top":"30px",
-//           "left": "450px",
-//         });
-//       })
-//     })
-// $(function(){
-//       $(".button_7").click(function(){
-//         $(".botanical_7").css({
-//           "opacity":"1",
-//           "transition":"0.7s",
-//           "top":"80px",
-//           "left": "400px",
-//         });
-//       })
-//     })
-// $(function(){
-//       $(".button_8").click(function(){
-//         $(".botanical_8").css({
-//           "opacity":"1",
-//           "transition":"0.7s",
-//           "top":"50px",
-//           "left": "400px",
-//         });
-//       })
-//     })
-// $(function(){
-//       $(".button_9").click(function(){
-//         $(".botanical_9").css({
-//           "opacity":"1",
-//           "transition":"0.7s",
-//           "top":"80px",
-//           "left": "500px",
-//         });
-//       })
-//     })
-//
-// /*非表示*/
-//
-// $(function(){
-//       $(".botanical_1").click(function(){
-//         $(this).css({
-//           "opacity":"0",
-//           "transition":"0.7s"
-//         });
-//       })
-//     })
 
 var buttonToFlowerConf = {
 	"button_1" : "botanical_1",
@@ -135,18 +33,21 @@ var buttonToFlowerConf = {
 
 $(document).ready(function(){
 
+	console.log($('.container').offset().left);
 	// $(".botanical_1").click(function(){
 	$('.botanical').click(function(){
 			$('.customizeImg').animate({opacity:0}, 1000);
 			$('.customizeImg').animate({scale:"0"}, 10000);
-
+			$('.customizeImgBouquetHandle').animate({opacity:1}, 2000);
 			$(this).css({"display":"none"});
 
 			var targetClass = buttonToFlowerConf[ $(this).attr("class").split(" ")[1]];
 			console.log( $(this).attr("class").split(" ")[1]);
 
 			console.log(targetClass);
-			var path = $.bezierCurve(180, 430).addPoint(230, 80).addPoint(250, 200);
+
+			// この３つの座標で、花のジャンプインの軌道を決定
+			var path = $.bezierCurve(80, 30).addPoint(130, -220).addPoint(280, -50);
 
 			//初期状態に設定
 			$('.' + targetClass).animate(
@@ -163,7 +64,7 @@ $(document).ready(function(){
 	    	bezierPath: path,
 				scale:'1',
 				opacity:'1'
-	 		}, 380, "easeInOutCubic");
+	 		}, 300, "easeInOutCubic");//300:花の移動時間(スピード)
 
 	});
 
@@ -177,42 +78,29 @@ $(document).ready(function(){
 
 		hammertime.on('rotate', function (ev) {
 
-			console.log("hogerotate");
+
 		});
 
 
-		// hammertime.on('panstart', function(ev){
-		// 	panstartX = ev.target.x;
-		// 	panstartY = ev.target.y;
-    //
-    //
-		// 	ev.target.style.top = panstartY;
-		// 	ev.target.style.left = panstartX;
-		// });
-    //
-		// hammertime.on('pan', function (ev) {
-		// 	 ev.preventDefault();
-		// 	 var target = ev.target;
-		// 	 target.style.top = ev.deltaY + (panstartY)  - 360 + 'px';
-		// 	 target.style.left = ev.deltaX + panstartX  + 'px';
-    //
-		// })
+		hammertime.on('panstart', function(ev){
+			panstartX = ev.target.x - Number($('.container').offset().left);
+			panstartY = ev.target.y;
+
+			ev.target.style.top = panstartY;
+			ev.target.style.left = panstartX ;
+		});
+
+
+
+		hammertime.on('pan', function (ev) {
+			 ev.preventDefault();
+			 var target = ev.target;
+			 target.style.top = ev.deltaY + panstartY  - 400 + 'px';
+			 target.style.left = ev.deltaX + panstartX  + 'px';
+
+		});
 	})
 
 	$('img').attr('onmousedown', 'return false');
 
 });
-
-/*    // Drag and Drop
-    $('.botanical').each(function (idx, $el) {
-      var hammertime = new Hammer($el)
-      hammertime.on('pan', function (ev) {
-        ev.preventDefault()
-        var target = ev.target;
-        target.style.top = ev.deltaY + 520 + 'px';
-        target.style.left = ev.deltaX + 380 + 'px';
-        target.style.position = 'absolute';
-        console.log(ev.deltaY);
-        console.log(ev.deltaX);
-      })
-    })*/
